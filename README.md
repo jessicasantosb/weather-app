a9160d37c99f7eacdad593948d58efdd8694fa6d
Jessica <jessica@pop-os.localdomain>
2023-07-04 09:57:35 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# weather-app
+
+<h1>It's a web application in JavaScript.</h1>
+
+<p>Doing the Weather App, I learned to use API and async functions.</p>
+
+<p>Live version of the site is <a href="https://jessicasantosb.github.io/weather-app/" target="_blank">here</a>.</p>
---
+++ script.js
@@ -0,0 +1,52 @@
+const apiKey = "b73d0e7bb40743bd67b6529abe9cbedd";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city");
+const temperature = document.querySelector("#temperature span");
+const description  = document.querySelector("#description");
+const weatherIcon  = document.querySelector("#weather-icon");
+const country = document.querySelector("#country-flag");
+const humidity = document.querySelector("#humidity span");
+const wind = document.querySelector("#wind span");
+
+const weatherData = document.querySelector("#weather-data");
+
+const getWeatherData = async (city) => {
+    const apiWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    const res = await fetch(apiWeatherUrl);
+    const data = await res.json();
+    return data
+};
+
+const showWeatherData = async (city) => {
+    const data = await getWeatherData(city);
+    if (data.name === undefined) {
+        alert("[ERROR] Please check the city name")
+    }
+    cityElement.innerText = data.name;
+    temperature.innerText = parseInt(data.main.temp);
+    description.innerText = data.weather[0].description;
+    weatherIcon.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    country.setAttribute("src", `https://flagsapi.com/${data.sys.country}/flat/64.png`);
+    humidity.innerText = `${data.main.humidity}%`;
+    wind.innerText = `${data.wind.speed} km/h`;
+    weatherData.classList.remove("hide")
+};
+
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const city = cityInput.value;
+    showWeatherData(city);
+});
+
+cityInput.addEventListener("keyup", (e) => {
+    if (e.code === "Enter") {
+        const city = e.target.value;
+        showWeatherData(city);
+    }
+});
+
+// SETUP LANGUAGE
+window.gtranslateSettings = {"default_language":"en","detect_browser_language":true,"languages":["en","pt","fr","de","it","es"],"wrapper_selector":".gtranslate_wrapper","alt_flags":{"pt":"brazil"}}
